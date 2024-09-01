6c9de1121436d432a50ae594dd4b3eea5a6260b7
Avery Yang(楊長珈) <avery_yang@asus.com>
2024-09-01 14:50:19 +0000

    feat: integrate Handlebars as the view engine
    - Set up Express Handlebars with .handlebars extension.
    - Configure view engine and views directory in Express.
    - Add main layout and home view templates.
    - Update package.json to include express-handlebars dependency.

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 import express from "express";
+import { join } from "path";
+import { create } from "express-handlebars";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Set Express handlebars as the view engine
+const exphdbs = create({
+  extname: ".handlebars",
+  defaultLayout: "main",
+  layoutsDir: join(process.cwd(), "views", "layouts"),
+  partialsDir: join(process.cwd(), "views", "partials"),
 });
 
+// Register the handlebars engine with Express
+app.engine("handlebars", exphdbs.engine);
+// Set the view engine to handlebars
+app.set("view engine", "handlebars");
+// Set the views directory
+app.set("views", join(process.cwd(), "views"));
+
+// Define a route for the root URL
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Hello, Handlebars with .handlebars extension!",
+  });
+});
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
