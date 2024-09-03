bb71503eb61fab9063612beb886122e41a01fd80
Avery Yang(楊長珈) <avery_yang@asus.com>
2024-09-03 12:03:58 +0000

    feat(models): add foreign key association between User and Todo models
    - Added `belongsTo` association in `Todo` model to link with `User` model using `userId` as foreign key.
    - Updated `User` model to include `hasMany` association with `Todo` model using `userId` as foreign key.

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -11,15 +9,19 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Todo.belongsTo(models.User, { foreignKey: "userId" });
     }
   }
-  Todo.init({
-    text: DataTypes.STRING,
-    completed: DataTypes.BOOLEAN,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      text: DataTypes.STRING,
+      completed: DataTypes.BOOLEAN,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Todo);
+      User.hasMany(models.Todo, { foreignKey: "userId" });
     }
   }
   User.init(
